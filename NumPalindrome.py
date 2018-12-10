0bf181c3662f328f1bb5e1b35362a413f9e822b1
davidgaribaldi <44825671+davidgaribaldi@users.noreply.github.com>
2018-12-10 12:44:48 -0800

    Add files via upload

---
+++ NumPalindrome.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 10 11:24:24 2018
+
+@author: davidg
+"""
+
+def NumericalPalindrome(num):
+    n = 0
+    length = len(num) - 1
+    for digit in num:
+        if digit != num[length - n]:
+            print('This is not a numerical palindrome')
+            break
+        else:
+            n += 1
+        if n == length:
+            print('the number:', num, 'is a numerical palindrome')
+            
+            
+input_Number = input("If you give me a number, I'll tell if you it's a palindrome: ")
+NumericalPalindrome(input_Number)
